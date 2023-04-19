22cb8401a8e69c4497b3c4b8c87948aee4f44b76
Nglav10 <nglaviano10@gmail.com>
2023-04-19 15:29:49 -0500

    feat: integrate server and client

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import { Layout } from '../components';
+import { Layout, QueryResult } from '../components';
+import { useQuery, gql } from "@apollo/client";
+import TrackCard from '../containers/track-card'
+
+/** TRACKS query to retrieve all tracks */
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <QueryResult data={data} loading={loading} error={error}>
+      <Layout grid>
+        {data?.tracksForHome?.map(track =>
+          <TrackCard key={track.id} track={track} />
+        )}
+      </Layout>
+    </QueryResult>
+  );
 };
 
 export default Tracks;
