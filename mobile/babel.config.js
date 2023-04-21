f593a5bec4d67048ae52277651a732aaa71bb483
Nglav10 <nglaviano10@gmail.com>
2023-04-20 20:31:57 -0500

    fix: remove path alias

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,16 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          '@components': 'mobile/ts/components',
-          '@api': 'mobile/ts/api',
-          '@typeDef': 'mobile/ts/typeDef.ts',
-          '@assets': 'mobile/assets',
-        },
-      },
-    ],
-  ],
 };
