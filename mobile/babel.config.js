24b831ba3a40a9dfb0f4b7fc5c9f4c8ac7679098
Nglav10 <nglaviano10@gmail.com>
2023-04-20 19:52:09 -0500

    feat: add path alias

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@components': 'mobile/ts/components',
+          '@api': 'mobile/ts/api',
+          '@typeDef': 'mobile/ts/typeDef.ts',
+          '@assets': 'mobile/assets',
+        },
+      },
+    ],
+  ],
 };
